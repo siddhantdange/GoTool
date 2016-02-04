7ec8f6a7ffde2e1f2a3da489613b13a461d3d5c6
Sid Dange <dange.siddhant@gmail.com>
2016-02-03 18:50:33 -0800

    added writing/reading to chrome storage

--- background.js
+++ background.js
@@ -1,17 +1,62 @@
 
 function resetDefaultSuggestion() {
-	  chrome.omnibox.setDefaultSuggestion({
-	     description : 'Go To:'
-	  });
+  chrome.omnibox.setDefaultSuggestion({
+     description : 'Go To:'
+  });
 }
 resetDefaultSuggestion();
 
 function navigate(url) {
-	  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-	     chrome.tabs.update(tabs[0].id, {url: url});
-	  });
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+     chrome.tabs.update(tabs[0].id, {url: url});
+  });
+}
+
+function saveShortcut(name, url, success, error) {
+
+   if (!name || !url) {
+     if (error) {
+       error('Error: No value specified');
+     }
+
+     return;
+   }
+
+   var pack = {};
+   pack[name] = url;
+   chrome.storage.sync.set(pack, function() {
+     if (success) {
+       success();
+     }
+   });
+}
+
+function getShortcut(name, callback) {
+  return chrome.storage.sync.get(name, function(url_dict) {
+    var len = Object.keys(url_dict).length;
+    if (!len) {
+      if (callback) {
+        callback(null);
+      }
+      return;
+    }
+
+    if (callback) {
+      callback(url_dict[name]);
+    }
+  });
 }
 
 chrome.omnibox.onInputEntered.addListener(function(text) {
-	  navigate("http://google.com");
+  saveShortcut('wheee', 'http://apple.com', function() {
+    console.log('saved!');
+  }, null);
+
+  getShortcut(text, function(url_back){
+    url = url_back;
+    if (!url) {
+      url = 'http://google.com';
+    }
+    navigate(url);
+  });
 });
